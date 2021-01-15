e78161236a3a691c25fa06b695daa946bc702268
golaj <ch.rehn90@gmail.com>
2021-01-15 09:45:00 +0100

    Add simple hello api

---
+++ SimpleRestAPI/src/main/java/com/example/SimpleRestAPI/MainRestController.java
@@ -0,0 +1,15 @@
+package com.example.SimpleRestAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class MainRestController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello there!";
+    }
+}
